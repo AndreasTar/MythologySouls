a1628f7e0538d29a8a6d76fe4e1e1e939adf8354
Andy Pandy <andytgamedev@gmail.com>
2022-10-25 11:55:27 +0300

    small remake using different method
    for some reason its very slow and jumping doesnt work (i know why jumping doesnt work, idk why its slow tho), even though in another project with the same code but with no jumping, it works fine. something with the jumping parameters messes it up. will have to look into it

--- MythologySoulsUnity/Assets/PlayerScripts/PlayerController.cs
+++ MythologySoulsUnity/Assets/PlayerScripts/PlayerController.cs
@@ -3,67 +3,64 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [Header("Speeds")]
-    [SerializeField] float moveSpeed;
+    [SerializeField] float moveSpeed = 5f;
 
     [Header("Physics")]
-    [SerializeField] float gravity;
+    [SerializeField] float gravity = -9.81f;
+    float finGravity;
     [SerializeField] float jumpHeight;
-    [SerializeField] float turnSmoothTime;
+    [SerializeField] float turnSmoothTime = 0.1f;
 
     [Header("External")]
     [SerializeField] CharacterController controller;
 
     [Header("Surface Control")]
-    [SerializeField] Transform groundCheck;
     [SerializeField] LayerMask groundMask;
-    [SerializeField] float groundDistance;
 
     [Header("Debug")]
     [SerializeField] bool debugMode;
-    Vector3 velocity;
+
+    Vector3 velocity = Vector3.zero;
     Vector3 direction = Vector3.zero;
     float turnSmoothVelocity;
-    bool grounded;
+    bool isGrounded;
 
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        grounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        Vector3 dist = new Vector3(0, .55f, 0);
+        isGrounded = Physics.CheckCapsule(transform.position - dist, transform.position + dist, 0.48f, groundMask);
         HandleGrounded(horizontal, vertical);
         //velocity.y += gravity * Time.deltaTime;
         //controller.Move(velocity * Time.deltaTime);
         HandleRotationSmoothing();
-        controller.SimpleMove((velocity + direction) * moveSpeed);
+        velocity.y += gravity * Time.deltaTime;
+        controller.SimpleMove((velocity + (direction * moveSpeed)) * Time.deltaTime);
     }
 
     void HandleGrounded(float horizontal, float vertical)
     {
-        if (grounded) 
-        { 
-            velocity.y = 0.0001f; 
-        }
-        if (!grounded && !debugMode)
-        {
-            return;
-        }
+        finGravity = isGrounded ? 0 : gravity;
+        velocity = isGrounded ? Vector3.zero : velocity;
+
+        if (!isGrounded && !debugMode) return;
+
         direction = new Vector3(horizontal, 0f, vertical).normalized;
         if (Input.GetButton("Jump"))
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            //velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            controller.SimpleMove(new Vector3(0, Mathf.Sqrt(jumpHeight * -2f * gravity), 0));
         }
     }
 
     void HandleRotationSmoothing()
     {
         if (!(direction.magnitude >= 0.1f)) return;
-        if (!grounded && !debugMode) return;
+        if (!isGrounded && !debugMode) return;
         float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
 
-        Mathf.SmoothDampAngle(
-            transform.eulerAngles.y,
-            targetAngle,
-            ref turnSmoothVelocity,
-            turnSmoothTime);
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
     }
 }
